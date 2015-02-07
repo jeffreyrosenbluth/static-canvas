f8035e6ce066cab9f97c1b77f7e83872b622e723
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2015-02-06 19:50:02 -0500

    addcolorstop

---
+++ src/Graphics/Static.hs
@@ -0,0 +1,112 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  Graphics.Static
+-- Copyright   :  (c) 2015 Jeffrey Rosenbluth
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  jeffrey.rosenbluth@gmail.com
+--
+-- DSL for creating HTML5 Canvas.
+--
+-------------------------------------------------------------------------------
+
+module Graphics.Static where
+
+import Data.Text                   (Text)
+import Graphics.Static.Internal
+import Control.Monad.Free          (liftF)
+
+arc :: Double -> Double -> Double -> Double -> Double -> Bool -> CanvasFree ()
+arc a1 a2 a3 a4 a5 a6 = liftF $ Arc a1 a2 a3 a4 a5 a6 ()
+
+arcTo :: Double -> Double -> Double -> Double -> Double -> CanvasFree ()
+arcTo a1 a2 a3 a4 a5 = liftF $ ArcTo a1 a2 a3 a4 a5 ()
+
+beginPath :: CanvasFree ()
+beginPath = liftF $ BeginPath ()
+
+bezierCurveTo :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree ()
+bezierCurveTo a1 a2 a3 a4 a5 a6 = liftF $ BezierCurveTo a1 a2 a3 a4 a5 a6 ()
+
+clearRect :: Double -> Double -> Double -> Double -> CanvasFree ()
+clearRect a1 a2 a3 a4 = liftF $ ClearRect a1 a2 a3 a4 ()
+
+clip :: CanvasFree ()
+clip = liftF $ Clip ()
+
+closePath :: CanvasFree ()
+closePath = liftF $ ClosePath ()
+
+fill :: CanvasFree ()
+fill = liftF $ Fill ()
+
+fillRect :: Double -> Double -> Double -> Double -> CanvasFree ()
+fillRect a1 a2 a3 a4 = liftF $ FillRect a1 a2 a3 a4 ()
+
+fillText :: Text -> Double -> Double -> CanvasFree ()
+fillText a1 a2 a3 = liftF $ FillText a1 a2 a3 ()
+
+globalAlpha :: Double -> CanvasFree ()
+globalAlpha a1 = liftF $ GlobalAlpha a1 ()
+
+globalCompositeOperation :: Text -> CanvasFree ()
+globalCompositeOperation a1 = liftF $ GlobalCompositeOperation a1 ()
+
+linearGradient :: Double -> Double -> Double -> Double -> CanvasFree Int
+linearGradient a1 a2 a3 a4 = liftF $ LinearGradient a1 a2 a3 a4 id
+
+lineTo :: Double -> Double -> CanvasFree ()
+lineTo a1 a2 = liftF $ LineTo a1 a2 ()
+
+lineWidth :: Double -> CanvasFree ()
+lineWidth a1 = liftF $ LineWidth a1 ()
+
+miterLimit :: Double -> CanvasFree ()
+miterLimit a1 = liftF $ MiterLimit a1 ()
+
+moveTo :: Double -> Double -> CanvasFree ()
+moveTo a1 a2 = liftF $ MoveTo a1 a2 ()
+
+quadraticCurveTo :: Double -> Double -> Double -> Double -> CanvasFree ()
+quadraticCurveTo a1 a2 a3 a4 = liftF $ QuadraticCurveTo a1 a2 a3 a4 ()
+
+radialGradient :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree Int
+radialGradient a1 a2 a3 a4 a5 a6 = liftF $ RadialGradient a1 a2 a3 a4 a5 a6 id
+
+rect :: Double -> Double -> Double -> Double -> CanvasFree ()
+rect a1 a2 a3 a4 = liftF $ Rect a1 a2 a3 a4 ()
+
+restore :: CanvasFree ()
+restore = liftF $ Restore ()
+
+rotate :: Double -> CanvasFree ()
+rotate a1 = liftF $ Rotate a1 ()
+
+save :: CanvasFree ()
+save = liftF $ Save ()
+
+scale :: Double -> Double -> CanvasFree ()
+scale a1 a2 = liftF $ Scale a1 a2 ()
+
+setTransform :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree ()
+setTransform a1 a2 a3 a4 a5 a6 = liftF $ SetTransform a1 a2 a3 a4 a5 a6 ()
+
+shadowBlur :: Double -> CanvasFree ()
+shadowBlur a1 = liftF $ ShadowBlur a1 ()
+
+shadowOffsetX :: Double -> CanvasFree ()
+shadowOffsetX a1 = liftF $ ShadowOffsetX a1 ()
+
+shadowOffsetY :: Double -> CanvasFree ()
+shadowOffsetY a1 = liftF $ ShadowOffsetY a1 ()
+
+stroke :: CanvasFree ()
+stroke = liftF $ Stroke ()
+
+strokeRect :: Double -> Double -> Double -> Double -> CanvasFree ()
+strokeRect a1 a2 a3 a4 = liftF $ StrokeRect a1 a2 a3 a4 ()
+
+transform :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree ()
+transform a1 a2 a3 a4 a5 a6 = liftF $ Transform a1 a2 a3 a4 a5 a6 ()
+
+translate :: Double -> Double -> CanvasFree ()
+translate a1 a2 = liftF $ Translate a1 a2 ()
--- src/Graphics/Static/Internal.hs
+++ src/Graphics/Static/Internal.hs
@@ -4,7 +4,7 @@
 
 -------------------------------------------------------------------------------
 -- |
--- Module      :  Graphics.Static.Types
+-- Module      :  Graphics.Static.Internal
 -- Copyright   :  (c) 2015 Jeffrey Rosenbluth
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  jeffrey.rosenbluth@gmail.com
@@ -13,16 +13,16 @@
 --
 -------------------------------------------------------------------------------
 
-module Graphics.Static.Types where
+module Graphics.Static.Internal where
 
-import           Control.Applicative
-import           Control.Monad.Free          (Free(..), liftF)
-import           Control.Monad.Free.Church   (F, fromF)
-import           Control.Monad.State
-import           Control.Monad.Writer
-import           Data.Double.Conversion.Text (toFixed)
-import           Data.Text                   (Text, pack)
-import           Data.Text.Lazy.Builder      (Builder, fromText)
+import Control.Applicative
+import Control.Monad.Free          (Free(..))
+import Control.Monad.Free.Church   (F, fromF)
+import Control.Monad.State
+import Control.Monad.Writer
+import Data.Double.Conversion.Text (toFixed)
+import Data.Text                   (Text, pack)
+import Data.Text.Lazy.Builder      (Builder, fromText, singleton)
 
 newtype Script a = Script {runScript :: (WriterT Builder (State Int) a)}
   deriving (Functor, Applicative, Monad, MonadWriter Builder, MonadState Int)
@@ -30,8 +30,8 @@ newtype Script a = Script {runScript :: (WriterT Builder (State Int) a)}
 type CanvasFree = F Canvas
 
 data Canvas r
-  = -- AddColorStop
-    Arc !Double !Double !Double !Double !Double !Bool r
+  = AddColorStop !Double !Color !Int r
+  | Arc !Double !Double !Double !Double !Double !Bool r
   | ArcTo !Double !Double !Double !Double !Double r
   | BeginPath r
   | BezierCurveTo !Double !Double !Double !Double !Double !Double r
@@ -81,6 +81,10 @@ data Canvas r
   | Translate !Double !Double r
     deriving Functor
 
+data Color = Hex !Text
+           | RGB !Int !Int !Int
+           | RGBA !Int !Int !Int !Double
+
 
 evalScript :: CanvasFree a -> Builder
 evalScript c = (evalState . execWriterT . runScript . eval . fromF) c 0
@@ -94,7 +98,37 @@ inc = do
   put (n + 1)
   return n
 
+comma :: Builder
+comma = comma
+
+jsBool :: Bool -> Builder
+jsBool True = "true"
+jsBool False = "false"
+
+jsInt :: Int -> Builder
+jsInt = fromText . pack . show
+
+jsDouble :: Double -> Builder
+jsDouble = fromText . toFixed 4
+
+jsColor :: Color -> Builder
+jsColor (Hex t) = fromText t
+jsColor (RGB r g b)    = "rgb("  <> (fromText . pack $ show r)
+                      <> comma   <> (fromText . pack $ show g)
+                      <> comma   <> (fromText . pack $ show b) <> singleton ')'
+jsColor (RGBA r g b a) = "rgba(" <> (fromText . pack $ show r)
+                      <> comma   <> (fromText . pack $ show g)
+                      <> comma   <> (fromText . pack $ show b)
+                      <> comma   <> (jsDouble a) <> singleton ')'
+
+
+--------------------------------------------------------------------------------
+
 eval :: Free Canvas a -> Script a
+eval (Free (AddColorStop a1 a2 a3 c)) = do
+  record ["gradient_", jsInt a3, ".addColorStop("
+         , jsDouble a1, comma, jsColor a2, ");"]
+  eval c
 eval (Free (Arc a1 a2 a3 a4 a5 a6 c)) = do
   record ["ctx.arc("
          , jsDouble a1, comma, jsDouble a2, comma
@@ -230,114 +264,3 @@ eval (Free (Translate a1 a2 c)) = do
   record ["translate(", jsDouble a1, comma, jsDouble a2, ");"]
   eval c
 eval (Pure x) = return x
-
-comma :: Builder
-comma = comma
-
-jsBool :: Bool -> Builder
-jsBool True = "true"
-jsBool False = "false"
-
-jsInt :: Int -> Builder
-jsInt = fromText . pack . show
-
-jsDouble :: Double -> Builder
-jsDouble = fromText . toFixed 4
-
--------------------------------------------------------------------------------
-
-arc :: Double -> Double -> Double -> Double -> Double -> Bool -> CanvasFree ()
-arc a1 a2 a3 a4 a5 a6 = liftF $ Arc a1 a2 a3 a4 a5 a6 ()
-
-arcTo :: Double -> Double -> Double -> Double -> Double -> CanvasFree ()
-arcTo a1 a2 a3 a4 a5 = liftF $ ArcTo a1 a2 a3 a4 a5 ()
-
-beginPath :: CanvasFree ()
-beginPath = liftF $ BeginPath ()
-
-bezierCurveTo :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree ()
-bezierCurveTo a1 a2 a3 a4 a5 a6 = liftF $ BezierCurveTo a1 a2 a3 a4 a5 a6 ()
-
-clearRect :: Double -> Double -> Double -> Double -> CanvasFree ()
-clearRect a1 a2 a3 a4 = liftF $ ClearRect a1 a2 a3 a4 ()
-
-clip :: CanvasFree ()
-clip = liftF $ Clip ()
-
-closePath :: CanvasFree ()
-closePath = liftF $ ClosePath ()
-
-fill :: CanvasFree ()
-fill = liftF $ Fill ()
-
-fillRect :: Double -> Double -> Double -> Double -> CanvasFree ()
-fillRect a1 a2 a3 a4 = liftF $ FillRect a1 a2 a3 a4 ()
-
-fillText :: Text -> Double -> Double -> CanvasFree ()
-fillText a1 a2 a3 = liftF $ FillText a1 a2 a3 ()
-
-globalAlpha :: Double -> CanvasFree ()
-globalAlpha a1 = liftF $ GlobalAlpha a1 ()
-
-globalCompositeOperation :: Text -> CanvasFree ()
-globalCompositeOperation a1 = liftF $ GlobalCompositeOperation a1 ()
-
-linearGradient :: Double -> Double -> Double -> Double -> CanvasFree Int
-linearGradient a1 a2 a3 a4 = liftF $ LinearGradient a1 a2 a3 a4 id
-
-lineTo :: Double -> Double -> CanvasFree ()
-lineTo a1 a2 = liftF $ LineTo a1 a2 ()
-
-lineWidth :: Double -> CanvasFree ()
-lineWidth a1 = liftF $ LineWidth a1 ()
-
-miterLimit :: Double -> CanvasFree ()
-miterLimit a1 = liftF $ MiterLimit a1 ()
-
-moveTo :: Double -> Double -> CanvasFree ()
-moveTo a1 a2 = liftF $ MoveTo a1 a2 ()
-
-quadraticCurveTo :: Double -> Double -> Double -> Double -> CanvasFree ()
-quadraticCurveTo a1 a2 a3 a4 = liftF $ QuadraticCurveTo a1 a2 a3 a4 ()
-
-radialGradient :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree Int
-radialGradient a1 a2 a3 a4 a5 a6 = liftF $ RadialGradient a1 a2 a3 a4 a5 a6 id
-
-rect :: Double -> Double -> Double -> Double -> CanvasFree ()
-rect a1 a2 a3 a4 = liftF $ Rect a1 a2 a3 a4 ()
-
-restore :: CanvasFree ()
-restore = liftF $ Restore ()
-
-rotate :: Double -> CanvasFree ()
-rotate a1 = liftF $ Rotate a1 ()
-
-save :: CanvasFree ()
-save = liftF $ Save ()
-
-scale :: Double -> Double -> CanvasFree ()
-scale a1 a2 = liftF $ Scale a1 a2 ()
-
-setTransform :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree ()
-setTransform a1 a2 a3 a4 a5 a6 = liftF $ SetTransform a1 a2 a3 a4 a5 a6 ()
-
-shadowBlur :: Double -> CanvasFree ()
-shadowBlur a1 = liftF $ ShadowBlur a1 ()
-
-shadowOffsetX :: Double -> CanvasFree ()
-shadowOffsetX a1 = liftF $ ShadowOffsetX a1 ()
-
-shadowOffsetY :: Double -> CanvasFree ()
-shadowOffsetY a1 = liftF $ ShadowOffsetY a1 ()
-
-stroke :: CanvasFree ()
-stroke = liftF $ Stroke ()
-
-strokeRect :: Double -> Double -> Double -> Double -> CanvasFree ()
-strokeRect a1 a2 a3 a4 = liftF $ StrokeRect a1 a2 a3 a4 ()
-
-transform :: Double -> Double -> Double -> Double -> Double -> Double -> CanvasFree ()
-transform a1 a2 a3 a4 a5 a6 = liftF $ Transform a1 a2 a3 a4 a5 a6 ()
-
-translate :: Double -> Double -> CanvasFree ()
-translate a1 a2 = liftF $ Translate a1 a2 ()
